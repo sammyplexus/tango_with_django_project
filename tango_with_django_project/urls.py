bacad3bc72d78c373d8c7a48975437a8c4ec2b90
Samuel Agbede <2370844a@student.gla.ac.uk>
2019-01-30 02:11:18 +0000

    Chapter 8 - Working with templates done!

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -19,10 +19,9 @@ from django.conf.urls import include
 
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-
 from rango import views
+
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
