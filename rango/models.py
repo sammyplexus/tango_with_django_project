641abbd50924ec722487ad1f1d9f89e8ae5910b9
Samuel Agbede <2370844a@student.gla.ac.uk>
2019-02-03 01:54:41 +0000

    Chapter 9 done - Cookies and Sessions

--- rango/models.py
+++ rango/models.py
@@ -1,7 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 from django.template.defaultfilters import slugify
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include
+    website = models.URLField(blank=True)
+    # The value of the upload_to is conjoined with the project's MEDIA_ROOT .../media/profile_images/
+    # The ImageField makes use of the Python Imaging Library (PIL)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Category(models.Model):
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -107,6 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = (
+   # 'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+   # 'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher'
+)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -131,3 +138,5 @@ STATICFILES_DIRS = [STATIC_DIR,]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+LOGIN_URL = '/rango/login/'
